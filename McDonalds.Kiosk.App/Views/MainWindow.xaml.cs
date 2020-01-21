db3ec6141532d77438ac64ba103872b9437dfa44
arale2900 <arale2900@gmail.com>
2020-01-21 20:37:34 +0100

    add MainWindow view

---
+++ McDonalds.Kiosk.App/Views/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using McDonalds.Kiosk.Core.Models;
+
+namespace McDonalds.Kiosk.App.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Page
+    {
+        List<Product> lstProductsInOffer = new List<Product>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LstProduct_Initialized(object sender, EventArgs e)
+        {
+            LstProducts.ItemsSource = lstProductsInOffer;
+        }
+
+        private void GetBurgers(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GetSides(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GetWraps(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GetColdDrinks(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GetHotDrinks(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ McDonalds.Kiosk.Utilities/TimeManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McDonalds.Kiosk.Core
+{
+    class TimeManager
+    {
+
+    }
+}
